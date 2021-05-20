d54c2503ba0acfbdbc92ee24f4c59a624e42cb18
digidak <induriprakash@gmail.com>
2021-05-20 10:24:54 +0530

    first commit

---
+++ rivi.php
@@ -0,0 +1,49 @@
+<?php
+ ini_set('display_errors','off');      
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                   
+    function get_data() {
+        $name = $_POST['name'];
+        $file_name='RiviData'. '.json';
+   
+        if(file_exists("$file_name")) { 
+            $current_data=file_get_contents("$file_name");
+            $array_data=json_decode($current_data, true);
+                               
+            $extra=array(
+                'Name' => $_POST['name'],
+                'Branch' => $_POST['branch'],
+                'Topic' => $_POST['topic'],
+                'Difficulty' => $_POST['difficulty'],
+                'Marks' => $_POST['marks'],
+                //'Year' => $_POST['year'],
+            );
+            $array_data[]=$extra;
+            echo "file exist<br/>";
+            return json_encode($array_data);
+        }
+        else {
+            $datae=array();
+            $datae[]=array(
+                'Name' => $_POST['name'],
+                'Branch' => $_POST['branch'],
+                'Topic' => $_POST['topic'],
+                'Difficulty' => $_POST['difficulty'],
+                'Marks' => $_POST['marks'],
+            );
+            echo "file not exist<br/>";
+            return json_encode($datae);   
+        }
+    }
+  
+    $file_name='RiviData'. '.json';
+      
+    if(file_put_contents("$file_name", get_data())) {
+        echo 'success';
+    }                
+    else {
+        echo 'There is some error';                
+    }
+}
+       
+?>
